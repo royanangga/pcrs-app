f04f0eb884360a4ebc28cb62129a2f1b00f3e2f0
royanangga <64944858+royanangga@users.noreply.github.com>
2026-06-30 15:10:53 +0700

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import TrackPage from './TrackPage.jsx'
 import './style.css'
+
+const path = window.location.pathname
+const trackMatch = path.match(/^\/track\/(.+)$/)
+
+const RootComponent = trackMatch
+  ? <TrackPage requestNo={decodeURIComponent(trackMatch[1])} />
+  : <App />
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {RootComponent}
   </React.StrictMode>,
 )
